290f7c67fd42853ebd7a850470ddc2138227f0a8
oyatomo <31516253+oyatomo@users.noreply.github.com>
2017-10-10 13:18:48 -0400

    Add files via upload

--- quizProgram/app.js
+++ quizProgram/app.js
@@ -4,6 +4,7 @@ If all answer are correct display massage saying 100%
 */
 
 var finish = document.getElementById("finish");
+var score = 0;
 
 finish.onclick = function () {
     'use strict';
@@ -11,6 +12,7 @@ finish.onclick = function () {
     var screen = document.getElementById("screen");
     if (screen.value === "16:9") {
         document.getElementById("demo").innerHTML = "O";
+        score = score + 1;
     } else {
         document.getElementById("demo").innerHTML = "X";
     }
@@ -18,6 +20,7 @@ finish.onclick = function () {
     var drive = document.getElementById("drive");
     if (drive.value === "Solid State") {
         document.getElementById("test").innerHTML = "O";
+        score = score + 1;
     } else {
         document.getElementById("test").innerHTML = "X";
     }
@@ -25,6 +28,7 @@ finish.onclick = function () {
     var boot = document.getElementById("boot");
     if (boot.value === "Computer manual") {
         document.getElementById("test2").innerHTML = "O";
+        score = score + 1;
     } else {
         document.getElementById("test2").innerHTML = "X";
     }
@@ -32,7 +36,20 @@ finish.onclick = function () {
     var ip = document.getElementById("ip");
     if (ip.value === "NETSTAT") {
         document.getElementById("test3").innerHTML = "O";
+        score = score + 1;
     } else {
         document.getElementById("test3").innerHTML = "X";
+    }
+///////////////////////////////////////////////////////////
+    if (score === 4) {
+        alert("100%");
+    } else if (score === 3) {
+        alert("75%");
+    } else if (score === 2) {
+        alert("50%");
+    } else if (score === 1) {
+        alert("25%");
+    } else if (score === 0) {
+        alert("You did not get any correct");
     }
 };
