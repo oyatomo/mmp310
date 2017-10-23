78412737bc73021a59ca56f5e3c2ddd1f1aa6748
oyatomo <31516253+oyatomo@users.noreply.github.com>
2017-10-23 05:39:02 -0400

    Add files via upload

--- taskList/app.js
+++ taskList/app.js
@@ -1,6 +1,10 @@
 var add = document.getElementById("add");
 
 add.onclick = function () {
-    var txt = document.getElementById("txt");
-    document.getElementById("demo").innerHTML = txt;
+    var txt = document.getElementById("txt").value;
+    var tasks = document.getElementById("tasks")
+    var task =document.createElement("li");
+    
+    task.innerHTML = txt;
+    tasks.append(task);
 };
